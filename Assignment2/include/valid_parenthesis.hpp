d3e520c64b7984117191c68f2bf6544f62bf09b6
VivianMak <vivianmak.30@gmail.com>
2026-09-16 12:00:13 -0400

    separete isValid func for testing

---
+++ Assignment2/include/valid_parenthesis.hpp
@@ -0,0 +1,35 @@
+#include <unordered_map>
+#include <string>
+#include <optional>
+
+#include "structs.hpp"
+
+// No need to reuse func, declare once
+bool isValid(std::string& s){
+
+    // static - reuses the built map
+    static const std::unordered_map<char, char> map = 
+    { // key, value
+        {')', '('},
+        {']', '['},
+        {'}', '{'}
+    };
+
+    Structs::Stack<char> stack;
+
+    for (char c : s){
+        if (c == '(' || c == '{' || c == '['){
+            stack.push(c); // add to back of list
+        } else{
+            // Check mapping pair (could return char or null)
+            std::optional<char> key = stack.pop();
+
+            // if key has no value OR wrong mapping
+            if (!key || *key != map.at(c)){
+                return false;
+            }
+        }
+    }
+    // return when we have checked the entire string
+    return stack.isEmpty();
+}
--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -1,39 +1,5 @@
 #include <iostream>
-#include <unordered_map>
-#include <string>
-#include <optional>
-
-#include "structs.hpp"
-
-// No need to reuse func, declare once
-bool isValid(std::string& s){
-
-    // static - reuses the built map
-    static const std::unordered_map<char, char> map = 
-    { // key, value
-        {')', '('},
-        {']', '['},
-        {'}', '{'}
-    };
-
-    Structs::Stack<char> stack;
-
-    for (char c : s){
-        if (c == '(' || c == '{' || c == '['){
-            stack.push(c); // add to back of list
-        } else{
-            // Check mapping pair (could return char or null)
-            std::optional<char> key = stack.pop();
-
-            // if key has no value OR wrong mapping
-            if (!key || *key != map.at(c)){
-                return false;
-            }
-        }
-    }
-    // return when we have checked the entire string
-    return stack.isEmpty();
-}
+#include "valid_parenthesis.hpp"
 
 int main()
 {   
